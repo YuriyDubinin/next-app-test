f5078c015d6385100d76ee90f7079e9a2537e402
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-11-19 16:52:34 +0300

    Some SEO

--- src/app/(pages)/accounts/[id]/page.tsx
+++ src/app/(pages)/accounts/[id]/page.tsx
@@ -5,7 +5,26 @@ import { useParams, useRouter } from 'next/navigation';
 
 import styles from './styles/style.module.css';
 
-export default function AccountID() {
+// dynamic SEO
+// const fetchAccount = async (postId: string) => {
+//     const post = await fetch(`/api/accounts/${postId}`, {
+//         method: 'GET',
+//     });
+//     return post.json();
+// }
+
+// export async function generateMetadata({params}: any) {
+//     const {account} = await fetchAccount(params.id)
+
+//     return {
+//         title: account[0].title,
+//         description: account[0].description
+//     }
+// }
+
+export default function Account() {
+    // fetch from here with the params id
+
     const params = useParams();
     const router = useRouter();
 
