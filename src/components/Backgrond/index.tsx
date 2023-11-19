5dbe075937ef82dea9c823708baa8028d9104bb4
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-11-19 21:18:27 +0300

    add

--- src/components/Backgrond/index.tsx
+++ src/components/Backgrond/index.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
 import background from '../../../public/background-main.jpg';
-import './styles/index.css';
+import './styles/style.css';
 
 export default function Background(props: any) {
     return (
-        <div className="main-background">
+        <div className="mainBackground">
             <Image src={background} width={1000} height={500} alt="main background" />
         </div>
     );
